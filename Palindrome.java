3ef3b6061201db43c209da06b5ea72146a3af42c
vaibhav2118 <vaibhavsalegave@gmail.com>
2026-04-17 13:30:19 +0530

    Add Palindrome.java implementation

---
+++ Palindrome.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string");
+        String s = sc.next();
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--)
+            rev = rev + s.charAt(i);
+        if (s.equals(rev))
+            System.out.println("String is palindrome");
+        else
+            System.out.println("String is not palindrome");
+
+    }
+
+}
